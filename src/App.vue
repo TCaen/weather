ebdac7313a3425c83104b055364f1bfb5a3cc957
thomas CAEN <thomascaen@hotmail.fr>
2020-11-01 16:32:26 +0100

    new

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,88 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app'>
+    <main>
+      <div class="search-box">
+        <input type="text" class='search-bar' placeholder="Seach...">
+      </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Northampton, UK</div>
+          <div class="date"> sunday 1 october 2020</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">9°</div>
+          <div class="weather">Rain</div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      api_key: '638ada882ee852924931438eb0e74c25'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body{
+  font-family: 'monsserat', sans-serif;
+}
+
+#app{
+  background-image: url('./assets/warm-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.5s;
+}
+
+main{
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75));
+}
+
+.search-box{
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar{
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255,255,255,0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus{
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255,255,255,0.75);
+  border-radius: 16px 0px 16px 0px;
 }
 </style>
+
